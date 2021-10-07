c53a2bf256642d6391b9b9ffefd7dceb3eec56cb
MateoSaez <79751192+MateoSaez@users.noreply.github.com>
2021-10-07 10:55:22 +0200

    prueba2

--- data.py
+++ data.py
@@ -11,7 +11,7 @@ class Data:
             'Europe': 'EUR',
             'Swiss': 'CHF',
             'UK': 'GBP',
-            'Japan': 'fghJPY',
+            'Japan': 'JPY',
             'Canada': 'CAD',
             'Pacific': 'AUD',
             'USA': 'USD'
